1e65987d49361334afe1f97cf5cbad58b1249b2e
Oleksandr <stranger.if.2014@gmail.com>
2016-05-23 14:48:50 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// JavaScript File
+
+var obj = {
+    name : "Username",
+    age : 20,
+    salary : 2000,
+};
+
+$(document).ready(function(){
+   var json = JSON.stringify(obj); 
+   
+    $("#toJson").click(function(){
+        $("#obj").append(json);
+    });
+    
+    var jsObject = JSON.parse(json);
+    
+    $("#toJSObject").click(function(){
+        console.log(jsObject);
+      
+    });
+    
+    
+});
